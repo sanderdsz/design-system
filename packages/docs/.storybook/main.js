fc6e0b1df70a654d99325c8b84df19307c0ee23d
Sander <sanderdsz@gmail.com>
2022-10-18 16:05:43 -0300

    ci: subpath fix

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
   "stories": [
     "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/**/*.stories.tsx"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
